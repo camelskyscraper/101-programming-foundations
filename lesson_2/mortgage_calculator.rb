d0b580d41b7d49701abd579d23f9a52b813c82be
Kirk Roberts <4321kirk@gmail.com>
2017-01-24 09:40:21 -0500

    rubocopped mortgage_calculator.rb

--- lesson_2/mortgage_calculator.rb
+++ lesson_2/mortgage_calculator.rb
@@ -34,7 +34,6 @@ Calculate the monthly payment
 Output the montly payment
 Output the number of months
 
-
 http://stackoverflow.com/questions/3730019/why-not-use-double-or-float-to-represent-currency
 tells me to use an integer with cents to represent money
 e.g. 1025 = $10.25
@@ -45,52 +44,49 @@ def prompt(message)
   puts "$-> #{message}"
 end
 
-def is_integer?(input)
-  input == input.to_i.to_s
-end
-
-def is_valid_number?(number)
-  not [0, 0.0].include?(number)
+def valid_number?(number)
+  ![0, 0.0].include?(number)
 end
 
 def format_money(float)
-  '%.2f' % float
+  format('%.2f', float)
 end
 
 def display_money(value_in_cents)
   "$" + (value_in_cents.to_i * 0.01).to_s
 end
 
-def get_float
+def input_float
   loop do
     input = gets.chomp().to_f
-    if is_valid_number?(input)
-      return input
-    end
+    return input if valid_number?(input)
     prompt("Hmm... that number was invalid. Please try again.")
   end
 end
 
-def get_monthly_payment(loan_amount, monthly_interest_rate, duration_in_months)
+def get_monthly_payment(loan_amount, j, n)
   # m = p * (j / (1 - (1 + j)**(-n)))
-  loan_amount * (monthly_interest_rate / (1 - (1 + monthly_interest_rate)**(-duration_in_months)))
+  # m: monthly payment
+  # p: loan amount
+  # j: monthly interest rate
+  # n: loan duration in months
+  loan_amount * (j / (1 - (1 + j)**-n))
 end
-
 
 puts("---")
 prompt("MORTGAGE CALCULATOR TIME!")
 
 prompt("What is your total loan amount? (numbers only, please)")
-loan_amount = get_float
+loan_amount = input_float
 prompt("Okay, great. Your loan amount is $#{format_money(loan_amount)}.")
 
 prompt("Now, what is your Annual Percentage Rate (APR)?")
 prompt("Please enter numbers only, as percentage points (e.g. 5 for 5%)")
-apr_display = get_float
+apr_display = input_float
 prompt("Okay, your APR is #{apr_display}%")
 
 prompt("What is the length of your loan (in years)?")
-years = get_float
+years = input_float
 years_display = years.to_i
 prompt("Got it, your loan length is #{years_display} years.")
 
@@ -98,9 +94,11 @@ prompt("Got it, your loan length is #{years_display} years.")
 apr = apr_display * 0.01
 months = years * 12.0
 monthly_interest_rate = apr / 12.0
-monthly_payment = get_monthly_payment(loan_amount, monthly_interest_rate, months)
+monthly_payment = \
+  get_monthly_payment(loan_amount, monthly_interest_rate, months)
 
 prompt("Calculating your numbers now...")
 sleep 2
 
-prompt("Looks like you'll be paying $#{format_money(monthly_payment)} per month for #{months.to_i} months.")
+prompt("Looks like you'll be paying $#{format_money(monthly_payment)}" \
+" per month for #{months.to_i} months.")
